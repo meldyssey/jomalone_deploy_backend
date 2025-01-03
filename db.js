93e46b0bb625f20319805eb3ece1bfcfeac54b8a
meldyssey <meldyssey15@gmail.com>
2025-01-03 23:15:49 +0900

    db 정보 수정

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
-const db = require('mysql2');
+const db = require("mysql2");
 
 const con = db.createConnection({
-    host: 'localhost',
-    user: 'jomalone',
-    password: '123456',
-    database: 'jomalone_db',
+    host: "mariadb",
+    port: "3306",
+    user: "root",
+    password: "123456",
+    database: "jomalone_db",
 });
 
-process.on('SIGINT', () => {
-    con.end(err => {
+process.on("SIGINT", () => {
+    con.end((err) => {
         if (err) {
-            console.log('종료 실패 : ', err.message);
+            console.log("종료 실패 : ", err.message);
         } else {
-            console.log('종료 성공 : ', con.threadId);
+            console.log("종료 성공 : ", con.threadId);
         }
 
         process.exit(0);
